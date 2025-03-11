603ccf985b24a9e81eb41800fb5411f1bf3c8226
Higor <higorestevao02@hotmail.com>
2025-03-11 16:21:10 -0300

    test: Incluido teste para Login

--- WebApplication1/Tests/Controllers/UserControllersTests.cs
+++ WebApplication1/Tests/Controllers/UserControllersTests.cs
@@ -1,10 +1,13 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http.Connections;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using WebApplication1.Controllers;
 using WebApplication1.Data;
 using WebApplication1.Dtos.Users;
 using WebApplication1.Models;
 using Xunit;
+using BCrypt;
 
 namespace WebApplication1.Tests.Controllers
 {
@@ -21,12 +24,13 @@ namespace WebApplication1.Tests.Controllers
                 Senha = "12345"
             };
 
-            // Configurando o banco de dados em memória para os testes
-            var options = new DbContextOptionsBuilder<AppDbContext>()
-                .UseInMemoryDatabase(databaseName: "Testes")
-                .Options;
+            // Configurando o banco de dados em memória para os teste
+            using var context = new AppDbContext(
+                new DbContextOptionsBuilder<AppDbContext>()
+        .UseInMemoryDatabase(databaseName: "Testes")
+        .Options
+);
 
-            using var context = new AppDbContext(options);
 
             // Criando uma instância do UserController com o contexto
             var controller = new UserController(context);
@@ -41,6 +45,55 @@ namespace WebApplication1.Tests.Controllers
             var user = Assert.IsType<Users>(createdResult.Value);
             Assert.Equal("Higor", user.Nome);
             Assert.Equal("Teste@gmail.com", user.Email);
+        }
+
+        [Fact]
+        public async Task LoginUser_ShouldReturn_OK_WhenLogged()
+        {
+            // Arrange
+            var dto = new LoginDto
+            {
+                Email = "Higor@gmail.com",
+                Senha = "123"
+            };
+
+
+            //database na memoria
+            var options = new DbContextOptionsBuilder<AppDbContext>()
+                .UseInMemoryDatabase(databaseName: "Testes")
+                .Options;
+
+            var context = new AppDbContext(options);
+
+            
+            //add user
+            context.Add(new Users
+            {
+                Nome = "Higor",
+                Email = "Higor@gmail.com",
+                SenhaHash = BCrypt.Net.BCrypt.HashPassword(dto.Senha),
+                RA = "20254952"
+
+
+            });
+
+            await context.SaveChangesAsync();
+
+
+            var controller = new UserController(context);
+
+
+            //Act
+            var result = await controller.LoginUser(dto);
+
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            Assert.NotNull(okResult.Value);
+
+            var responseObject = okResult.Value;
+            Assert.Equal("Login efetuado com sucesso", responseObject.GetType().GetProperty("mensagem")?.GetValue(responseObject, null));
+
+
+
         }
 
     }
